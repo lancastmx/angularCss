82e23d03ff9e4740eadd57d36669dcf5c55b4314
Angel <acastillo@prayser.com>
2024-09-09 10:59:32 -0600

    feat:primer archivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angularCss';
+  isActive = false; // Controla si el botón está activo
+  isHovered = false; // Controla si el botón está en estado hover
+  isDisabled = false; // Controla si el botón está deshabilitado
+
+  // Cambiar el estado de "activo" al hacer clic
+  toggleActive() {
+    if (!this.isDisabled) {
+      this.isActive = !this.isActive;
+    }
+  }
+
+  // Deshabilitar el botón (por ejemplo, después de un clic)
+  disableButton() {
+    this.isDisabled = true;
+  }
 }
